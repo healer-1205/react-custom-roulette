b248d637592093091f2753324fa7c874f6299ffb
healer-1205 <crazyhealer1205@gmail.com>
2023-05-06 01:26:50 +0800

    start project with typescript and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <p className="text-3xl font-bold underline">Hello</p>
+    </div>
+  )
+}
+
+export default App
