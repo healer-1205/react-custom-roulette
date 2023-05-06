5aae3ec24d78cd356dce22634fd9dbc514bddfd9
healer-1205 <crazyhealer1205@gmail.com>
2023-05-06 08:02:48 +0800

    make roulette

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
-import { Header } from "./components/Header"
 import { Home } from "./pages/Home"
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
 import React from "react"
+import { DraggeableForm } from "../../components/DraggeableForm"
 
 export const Home = () => {
-  return <div></div>
+  return (
+    <div className="App">
+      <DraggeableForm />
+    </div>
+  )
 }
